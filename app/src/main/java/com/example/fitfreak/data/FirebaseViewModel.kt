341ec9132b20d4e8673b1b85565aa171e6c23f7b
rG33-dev <rg3322@proton.me>
2026-02-14 17:03:21 +0530

    checking for authentication bug

---
+++ app/src/main/java/com/example/fitfreak/data/FirebaseViewModel.kt
@@ -0,0 +1,81 @@
+package com.example.fitfreak.data
+
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+
+
+class AuthViewModel : ViewModel() {
+    private val auth = FirebaseAuth.getInstance()
+
+    // This state tells the UI if the user is logged in or not
+    private val _authState = mutableStateOf<AuthState>(AuthState.Idle)
+    val authState: State<AuthState> = _authState
+
+
+    init {
+        checkAuthStatus()
+    }
+
+    fun checkAuthStatus() {
+        if (auth.currentUser != null) {
+            _authState.value = AuthState.Authenticated
+        } else {
+            _authState.value = AuthState.Unauthenticated
+        }
+    }
+
+    fun login(email: String, password: String) {
+        _authState.value = AuthState.Loading
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    _authState.value = AuthState.Authenticated
+                } else {
+                    _authState.value = AuthState.Error(task.exception?.message ?: "Unknown error")
+
+
+                }
+            }
+
+    }
+
+    fun SignUp(email: String, password: String) {
+        _authState.value = AuthState.Loading
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    _authState.value = AuthState.Authenticated
+                } else {
+                    _authState.value = AuthState.Error(task.exception?.message ?: "Unknown error")
+
+                }
+            }
+
+    }
+
+
+
+
+
+
+
+    fun logout() {
+        auth.signOut()
+        _authState.value = AuthState.Unauthenticated
+    }
+
+}
+
+
+
+// Define the possible states for the user
+sealed class AuthState {
+    object Idle : AuthState()
+    object Loading : AuthState()
+    object Authenticated : AuthState()
+    object Unauthenticated : AuthState()
+    data class Error(val message: String) : AuthState()
+}
